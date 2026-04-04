365b81243f0be21e2f7c66db009c73fc4440ac54
RAVI KUMAR (WIPRO LIMITED) <v-kumarravi5@microsoft.com>
2026-04-04 12:37:38 +0530

    Initial commit

---
+++ API/Data/SQL_Query/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, DisplayName, Email)
+VALUES ("bob-id", "Bob", "bob@test.com");
+
+INSERT INTO Users (Id, DisplayName, Email)
+VALUES ("Tom-id", "Tom", "tom@test.com");
+
+INSERT INTO Users (Id, DisplayName, Email)
+VALUES ("Jane-id", "Jane", "jane@test.com");
