206a6d0f5bac468c0d652f17629cef649168c2f7
RAVI KUMAR (Wipro Limited) <v-kumarravi5@microsoft.com>
2026-07-22 15:55:38 +0530

    Section 13: Paging, sorting and filtering

--- API/Interfaces/iMemberRepository.cs
+++ API/Interfaces/iMemberRepository.cs
@@ -1,12 +1,13 @@
 using System;
 using API.Entities;
+using API.Helpers;
 namespace API.Interfaces;
 
 public interface IMemberRepository
 {
     void Update(Member member);
     Task<bool> SaveAllAsync();
-    Task<IReadOnlyList<Member>> GetMembersAsync();
+    Task<PaginatedResult<Member>> GetMembersAsync(MemberParams memberParams);
     Task<Member?> GetMemberByIdAsync(string id);
     Task<IReadOnlyList<Photo>> GetPhotosForMembersAsync(string memberId);
     Task<Member?> GetMemberForUpdate(string id);
