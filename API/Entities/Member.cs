b0cef2535e55de9989941a76866f2ce9857b5550
RAVI KUMAR (Wipro Limited) <v-kumarravi5@microsoft.com>
2026-07-31 13:40:25 +0530

    Section 15: Messaging

--- API/Entities/Member.cs
+++ API/Entities/Member.cs
@@ -25,6 +25,10 @@ public class Member
     
     [JsonIgnore]
     public List<MemberLike>LikedMembers { get; set; } = [];
+    [JsonIgnore]
+    public List<Message>MessagesSent { get; set; } = [];
+    [JsonIgnore]
+    public List<Message>MessagesReceived { get; set; } = [];
     
     [JsonIgnore]
     [ForeignKey(nameof(Id))]
